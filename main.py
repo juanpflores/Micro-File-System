331abb8a8938e17c015e2eb8fefd1cbe794e7d2a
Diego Alberto Barriga <umoqnier@gmail.com>
2016-05-17 17:45:40 -0500

    Creación de funcion unzip y validacion de la existencia del archivo .zip

--- main.py
+++ main.py
@@ -4,12 +4,34 @@
 
 # Import sysy as a standard library.
 import sys
-
+import os
 # Gather our code in a main() function
 def main():
-  print 'Hello Diego!', sys.argv[1]
+	print('Hello Diego!'), sys.argv[1]
+	os.system("clear")
+	os.system("ls | grep FILE_SYSTEM.zip > test")
+	archivo = open("test", "r")
+	find_zip = archivo.readline()
+	if find_zip == "FILE_SYSTEM.zip\n":
+		print("La carpeta existe")
+		print("====Micro System Morgan====")
+		unzip(find_zip)
+		print("Lista de Comandos: newdir, newfile, removedir, removefile")
+		os.system("cd FILE_SYSTEM/") #No entra a la carpeta
+		os.system("pwd")
+		comando = input()
   # Command line args are in sys.argv[1], sys.argv[2] ..
   # sys.argv[0] is the script name itself and can be ignored
+
+def unzip(file_zip):
+
+	if len(file_zip) > 0:
+		os.system("unzip FILE_SYSTEM.zip")
+		print("Se descomprimio el zip")
+		os.system("ls")
+	else:
+		print("No existe el zip")
+
 
 # Standard boilerplate to call the main() function to begin
 # the program.
