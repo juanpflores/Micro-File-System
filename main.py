1da947e7e2bfdcac1d62b4b08e2665f1c4aff11c
Diego Alberto Barriga <umoqnier@gmail.com>
2016-05-18 00:14:50 -0500

    Funcion remove-file funcionando, nuevos comandos y se quitaron impresiones para ver flujo del programa

--- main.py
+++ main.py
@@ -9,30 +9,39 @@ import os
 def main():
 	print('Hello Diego!'), sys.argv[1]
 	os.system("clear")
-	os.system("ls | grep FILE_SYSTEM.zip > test")
-	archivo = open("test", "r")
+	os.system("ls | grep FILE_SYSTEM.zip > tmp")
+	archivo = open("tmp", "r")
 	find_zip = archivo.readline()
 	if find_zip == "FILE_SYSTEM.zip\n":
 		print("La carpeta existe")
-		print("====Micro System Morgan====")
+		print("========================================Micro System Morgan========================================")
 		unzip(find_zip)
-		print("Lista de Comandos: newdir <name>, newfile <name>, removedir <name>, removefile <name>, edit <file>, goin <name>, goback, list, help")
+		print("Lista de Comandos: newdir <name>, newfile <name>, removedir <name>, removefile <name>, edit <file>, read <file>, goin <name>, goback, list-items, exit, help")
 		os.chdir("FILE_SYSTEM")
-		os.system("pwd")
 		comando = input()
+		#if comando[:10] == "removefile":
+		#	remove_file(comando[10:]) <--- Probando la funcion
 	else:
 		print("NOT FOUND =(")
   # Command line args are in sys.argv[1], sys.argv[2] ..
   # sys.argv[0] is the script name itself and can be ignored
 
 def unzip(file_zip):
-
 	if len(file_zip) > 0:
 		os.system("unzip FILE_SYSTEM.zip")
 		print("Se descomprimio el zip")
-		os.system("ls")
 	else:
 		print("No existe el zip")
+
+def remove_file(file):
+	os.system("ls | grep "+ file +" > tmp")
+	archivo = open("tmp", "r")
+	find_file = archivo.readline()
+	if len(find_file) > 0:
+		os.system("rm -rf " + find_file)
+		print("DONE!!!")
+	else:
+		print("FILE NOT FOUND")
 
 
 # Standard boilerplate to call the main() function to begin
